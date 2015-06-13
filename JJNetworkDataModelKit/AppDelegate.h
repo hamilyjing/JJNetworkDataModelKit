54f5a4cad155ec8e8d954e8570c3e76d99b810d2
JJ <gongjian_001@126.com>
2015-06-13 16:20:30 +0800

    Initial Commit

---
+++ JJNetworkDataModelKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JJNetworkDataModelKit
+//
+//  Created by gongjian03 on 6/13/15.
+//  Copyright (c) 2015 JJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JJNetworkDataModelKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JJNetworkDataModelKit
+//
+//  Created by gongjian03 on 6/13/15.
+//  Copyright (c) 2015 JJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
